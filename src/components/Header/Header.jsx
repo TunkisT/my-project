c70645b4de8516f7a3f5fa2c4d1c062446d62023
Tautvydas Tunkis <ttunkis@gmail.com>
2022-06-03 13:08:39 +0300

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as S from './Header.style';
+
+function Header(props) {
+  return (
+    <div>
+      <S.Header>divas</S.Header>
+    </div>
+  );
+}
+
+Header.propTypes = {};
+
+export default Header;
---
+++ src/components/Header/Header.style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import image from '../../images/image1.jpg';
+
+export const Header = styled.header`
+  width: 100%;
+  height: 25rem;
+  opacity: 0.8;
+  background-image: url(${image});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;

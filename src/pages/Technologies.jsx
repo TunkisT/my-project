795a141ea3c5312861f98011dc46ab55d2ec1629
Tautvydas Tunkis <ttunkis@gmail.com>
2022-07-12 13:52:49 +0300

    tech

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -7,11 +7,11 @@ const Technologies = () => {
   return (
     <Header>
       <Container main='Mostly used technologies in my projects'>
+        <Body image='https://sohamkamani.github.io/assets/images/posts/express-internals/express-routing-logo.png' />
         <Body image='https://logos-download.com/wp-content/uploads/2016/09/Node_logo_NodeJS-700x428.png' />
         <Body image='https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png' />
         <Body image='https://miro.medium.com/max/318/1*p1TndLk3UsGPBsM7qHPZIw.png' />
-        <Body image='https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png' />
-        <Body image='https://jimstrother.com/img/skills/bCrypt.jpg' />
+        <Body image='https://pngimg.com/uploads/mysql/mysql_PNG14.png' />
       </Container>
     </Header>
   );
